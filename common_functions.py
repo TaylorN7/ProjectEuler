1cdc6ee7a552d76f5d0c25de72a93902e5e0692c
tneves <taylornit7@outlook.com>
2020-04-06 11:01:00 -0700

    updated

--- common_functions.py
+++ common_functions.py
@@ -8,13 +8,17 @@ def get_primes(c,n):
 	#c = int(input('What number should I start with?\n')) 
 	#n = int(input('What number should I go to?\n')) 
 	prime_list = []
+
+	# Added this IF at Problem #50 - Remove if necessary for problems before that
+	if 2 >= c or 2 <= n:
+		prime_list.append(2)
 	
 	for num in range(c, n, 2):
 		if all(num%i!=0 for i in range(2, int(math.sqrt(num))+1)):
 			
 			#print('Number: ' + str(num))
 			prime_list.append(num)
-	
+
 	return prime_list
 
 # Common Function: 02
