ad837a6763c61c309863ec3d51ce586f903e3951
tneves <taylornit7@outlook.com>
2020-04-17 09:11:21 -0700

    New try for 51

---
+++ Problem_051_v2.py
@@ -0,0 +1,14 @@
+# Project Euler - Problem #51 Prime Digit Replacements
+
+import common_functions
+from datetime import datetime
+startTime = datetime.now()
+
+
+
+
+
+
+print('\nDuration: ')
+print(datetime.now() - startTime)
+
